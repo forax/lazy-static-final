3c266eb3ad60af36e00d105a26affa1c154f7fb8
forax <forax@univ-mlv.fr>
2019-03-27 14:57:10 +0100

    don't print anything

--- src/main/java/fr.umlv.lazystaticfinal/fr/umlv/lazystaticfinal/Hello.java
+++ src/main/java/fr.umlv.lazystaticfinal/fr/umlv/lazystaticfinal/Hello.java
@@ -2,6 +2,6 @@ package fr.umlv.lazystaticfinal;
 
 public class Hello {
   public static void main(String[] args) {
-    System.out.println("hello world");
+    //System.out.println("hello world");
   }
 }
